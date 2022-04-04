4976cc27bededef8593a5004e99489ddf5277b05
minjee412 <minjee412@gmail.com>
2022-04-04 14:51:39 +0900

    2022.04.04

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- HTML -->
+  <header>
+    <h1>TODO IT!</h1>
+  </header>
+</template>
+
+<script>
+// Javascript
+export default {
+  
+}
+</script>
+
+<style scoped>
+/* CSS */
+h1 {
+  color:#2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="inputBox shadow">
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo"> 
+      <!-- v-on:keyup.enter 는 enter를 눌렀을때 동작하는 메서드 설정 -->
+      
+      <!-- <button v-on:click="addTodo">add</button> -->
+      <span class="addContainer"  v-on:click="addTodo">
+          <i class="fa-solid fa-plus addBtn"></i>
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+    data:function(){
+        return{
+            newTodoItem: ""
+        }
+    },
+
+    methods:{
+        addTodo: function(){
+            // console.log(this.newTodoItem);
+
+            // localstorage에 저장하는 로직
+            localStorage.setItem(this.newTodoItem, this.newTodoItem); //localStorage.setItem(key, value); 
+            this.clearInput();
+        },
+        clearInput: function(){
+            this.newTodoItem='';
+        }
+    }
+}
+</script>
+
+// style의 scoped : 해당 컴포넌트 안에서만 적용 되도록 설정하는 것
+<style scoped>
+
+input:focus {
+    outline: none;
+}
+
+.inputBox {
+    background-color: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
+
+</style>
